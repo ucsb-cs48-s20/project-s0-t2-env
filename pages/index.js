e52081fb748d24aeb2d9eaff247627bd9ff26475
zacharyfriedland <zacharyfriedland@ucsb.edu>
2020-04-19 13:18:14 -0700

    zf - playing around with basic formatting for homepage

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ export default function Index() {
         <div>
             <title>Environmental Impacts Dashboard</title>
 
-            <h1>Welcome to the Environmental Impacts Dashboard</h1>
+            <h1 className="header">Welcome to the Environmental Impacts Dashboard</h1>
 
-            <h2>Next.js</h2>
+            <h2 className="subhead2">Next.js</h2>
             <p>
                 This application provides a dashboard of information about your city's environmental impact.
                 When possible, the app shows you things like: amount of car traffic, public transportation options, news about policies enacted.
@@ -17,10 +17,11 @@ export default function Index() {
                 </ul>
             </p>
 
-            <h3>Goleta Environmental Data</h3>
+            <h3 className="subhead3">Goleta Environmental Data</h3>
             <ul>
                 <p>
                     <b>Tap Water Quality Details: </b>
+                    <img src="https://www.gallatintn.gov/ImageRepository/Document?documentID=1624" align = "right" width="500" height="200"></img>
                     <ul>
                         <li>Goleta, California</li>
                         <li>Serves: 86,946</li>
@@ -30,6 +31,33 @@ export default function Index() {
                     </ul>
                 </p>
             </ul>
+
+            <style jsx> {`
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+                .header {
+                    color: white;
+                    text-shadow:
+		                -1px -1px 0 #000,
+		                1px -1px 0 #000,
+		                -1px 1px 0 #000,
+		                1px 1px 0 #000;
+                    background-image: url('https://alliswall.com/file/2238/1920x1200/16:9/green-bamboo-background-wide.jpg');
+                    font-family: 'Montserrat', sans-serif;
+                    width: 1425px;
+                    height: 100px;
+                    text-align: center;
+                    line-height: 100px;
+                }
+                .subhead2 {
+                    color: #3269a8
+                }
+                .subhead3 {
+                    color: #3269a8
+                }
+
+
+            `}
+            </style>
         </div>
     );
 }
