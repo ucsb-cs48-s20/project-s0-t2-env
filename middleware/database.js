dce7d215c5e7a0bbf7002ac765b41fa5de306908
DARRAGH BURKE <darragh@ucsb.edu>
2020-04-28 14:50:18 -0700

    db fix caps issue and add comments

--- middleware/database.js
+++ middleware/database.js
@@ -1,11 +1,16 @@
 import { MongoClient } from "mongodb";
 import nextConnect from "next-connect";
 
+// this is a middleware. read more about middleware here: https://www.npmjs.com/package/connect
+
+// process.env.MONGO_CONNECTION_STRING is set in the .env file`
 const client = new MongoClient(process.env.MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// this function's purpose is just to add "dbClient" and "db" fields to the req object
+// we connect to MongoDB, then select the db called "environment"
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
--- pages/api/cities/[city].js
+++ pages/api/cities/[city].js
@@ -3,15 +3,25 @@ import middleware from "../../../middleware/database";
 
 const handler = nextConnect();
 
+// let the middleware process the request first, so that it can add the "db" field to req
 handler.use(middleware);
 
 handler.get(async (req, res) => {
+  // The city name is held in req.query.city, but we want to convert santa-barbara to "santa barbara"
   const city = req.query.city.replace("-", " ");
-  console.log(city);
+  // construct a query object for the database. we want to filter only results where the name = city
   const query = { name: city };
-  const doc = await req.db.collection("cities").findOne(query);
+  console.log(query);
+  // look for at most one city in the collection 'cities' using the query. By setting collation strength to 1, we are ignoring case
+  // https://mongodb.github.io/node-mongodb-native/2.2/tutorials/collations/#find-and-sort
+  const doc = await req.db
+    .collection("cities")
+    .findOne(query, { collation: { locale: "en_US", strength: 1 } });
+  // MongoDB returns a document for us
   console.log(doc);
+  // We can read in these values from the doc
   const { name, state, population, CO2, latitude, longitude } = doc;
+
   // TODO: get air quality
   const aqi = 31;
   // const response = await fetch("AIR-API");
@@ -21,6 +31,7 @@ handler.get(async (req, res) => {
   const waterQuality = 100;
   // const response = await fetch("WATER-API");
   // do something with it
+
   res.json({
     name,
     state,
