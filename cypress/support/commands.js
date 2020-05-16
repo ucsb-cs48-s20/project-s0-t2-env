77c5dd7d15fd2d0c8cec104bbd0fb3aef3bfceee
Noah Pang <noahpang@cs.ucsb.edu>
2020-05-16 15:21:03 -0700

    np - created new water script which doesn't download files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import guestUser from "../fixtures/guestUser.json";
+
+Cypress.Commands.add("loginAsGuest", () =>
+  cy.setCookie("AUTH", JSON.stringify(guestUser))
+);
