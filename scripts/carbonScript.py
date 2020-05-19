92827792486e668950bb52017b3c0059d86ab343
zacharyfriedland <zacharyfriedland@ucsb.edu>
2020-05-19 15:27:38 -0700

    zf - added number_of_cities variable and changed city to name and CO2_Emissions to CO2 for database

--- scripts/carbonScript.py
+++ scripts/carbonScript.py
@@ -1,11 +1,11 @@
-import pandas as pd
 from pymongo import MongoClient 
 from openpyxl import load_workbook
+from decouple import config
 
 try: 
-	client = MongoClient('mongodb+srv://guestuser:RktBAzVKMFoI1N7c@cluster0-qbnoy.mongodb.net/test?retryWrites=true&w=majority') 
+	client = MongoClient(config('MONGO_CONNECTION_STRING_FOR_TESTING')) 
 	envDb = client.environment
-	collection = envDb.citiesTest
+	collection = envDb.cities
 	print("Connected successfully!") 
 except: 
 	print("Could not connect to MongoDB") 
@@ -15,25 +15,54 @@ workbook = load_workbook(filename="Jones-Kammen-2014-Zip-City-County-Results.xls
 workbook.active = 2
 sheet = workbook.active
 idNum = 1
+# skip 1140 for {city: "CA"}, line 26672 is the last CA
 
-for row in sheet.iter_rows(min_row=2, max_row=26785, min_col=1, values_only=True):
+# if number_of_cities is 0, the script will run for all cities
+# if number_of_cities is given a number other than 0, the script will run for the number of cities designated
+
+number_of_cities = 0
+
+if(number_of_cities == 0):
+	number_of_cities = 26784
+
+number_of_cities+=1
+
+#26785 total rows
+
+for row in sheet.iter_rows(min_row=2, max_row=number_of_cities, min_col=1, values_only=True):
 	state = row[0]
 	county = row[1]
 	city = row[2]
 	co2 = row[16]
 	co2_household = row[14]
+	transport = row[9]
+	housing = row[10]
+	food = row [11]
+	goods = row[12]
+	services = row[13]
+	electricity = row[5]
+	natGas = row[6]
+	oil = row[7]
+	milesTraveled = row[8]
 
 	location = {
 		"_id":idNum, 
         "state":state, 
         "county":county,
-		"city":city,
-		"CO2_Emissions":co2,
-		"CO2_Emissions_Per_Household": co2_household
+		"name":city,
+		"CO2":co2,
+		"CO2_Emissions_Per_Household": co2_household,
+		"Transport": transport,
+		"Housing": housing,
+		"Food": food,
+		"Goods": goods,
+		"Services": services,
+		"Electricity": electricity,
+		"Natural_Gas": natGas,
+		"Fuel_Oil": oil,
+		"Vehicle_Miles_Traveled": milesTraveled
     }
 	collection.insert_one(location)
-	
-	
 	idNum+=1
 
 print("Complete")
