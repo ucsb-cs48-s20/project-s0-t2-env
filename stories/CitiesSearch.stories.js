635ac42c1dd740c0fd5a23f90745d966ecc7c8ba
Katelyn Zhang <krzhang@ucsb.edu>
2020-05-26 03:29:23 -0700

    kz - options.filter is still not found, and the search bar still takes a long time to load when running storybook

--- stories/CitiesSearch.stories.js
+++ stories/CitiesSearch.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CitiesSearch from "../components/CitiesSearch";
-import { select, text, object, array } from "@storybook/addon-knobs";
+import { select, text, object, array, number } from "@storybook/addon-knobs";
 // import { action } from "@storybook/addon-actions";
 import { AppBar, Toolbar } from "@material-ui/core";
 import useSWR from "swr";
 import { useTheme } from "@material-ui/core";
 import { createFilterOptions } from "@material-ui/lab/Autocomplete";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export default {
   title: "CitiesSearch",
@@ -14,24 +15,35 @@ export default {
 
 export const SimpleCitiesSearch = () => {
   const data = object(useSWR("/api/cities/all", fetch, {}));
+  //   const data = object(useSWR("/api/shortJSON.json", fetch, {}));
+  //   const data = object("Data", ["A", "B", "C", "D"]);
   const theme = object(useTheme());
-  const filter = object(
+  const filterOptions = object(
     createFilterOptions({
       limit: 5,
     })
   );
+  /*
+  const filName = text("FilName", "FilterOptions");
+  const limit = number("Limit", 5);
+  const filterOptions = object(filName, limit);
 
-  const options = array("Options", [1, 2, 3, 4, 5, 6, 7, 8], ",");
+  const opts = array("Options", [1, 2, 3, 4, 5, 6, 7, 8], ",");
+  */
 
   const value = text("Value", "San Francisco");
 
-  const names = { data };
-  const filterOptions = { options, filter };
+  const names = { data, filterOptions };
+  //   const filterOptions = { options, filter };
   return (
     <AppBar>
       <Toolbar>
         <div>
-          <CitiesSearch names={names} value={value} filterOptions={options} />
+          <CitiesSearch
+            names={names}
+            value={value}
+            filterOptions={filterOptions}
+          />
         </div>
       </Toolbar>
     </AppBar>
