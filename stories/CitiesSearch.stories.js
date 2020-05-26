51d1679bda3bb234d8db904c7046dc4d179a4465
Katelyn Zhang <krzhang@ucsb.edu>
2020-05-26 02:38:49 -0700

    kz - kinda works, search bar shows up, but clicking causes an options.filter is not a function error

--- stories/CitiesSearch.stories.js
+++ stories/CitiesSearch.stories.js
@@ -1,8 +1,11 @@
 import React from "react";
 import CitiesSearch from "../components/CitiesSearch";
-// import { select, text } from "@storybook/addon-knobs";
+import { select, text, object, array } from "@storybook/addon-knobs";
 // import { action } from "@storybook/addon-actions";
 import { AppBar, Toolbar } from "@material-ui/core";
+import useSWR from "swr";
+import { useTheme } from "@material-ui/core";
+import { createFilterOptions } from "@material-ui/lab/Autocomplete";
 
 export default {
   title: "CitiesSearch",
@@ -10,11 +13,25 @@ export default {
 };
 
 export const SimpleCitiesSearch = () => {
+  const data = object(useSWR("/api/cities/all", fetch, {}));
+  const theme = object(useTheme());
+  const filter = object(
+    createFilterOptions({
+      limit: 5,
+    })
+  );
+
+  const options = array("Options", [1, 2, 3, 4, 5, 6, 7, 8], ",");
+
+  const value = text("Value", "San Francisco");
+
+  const names = { data };
+  const filterOptions = { options, filter };
   return (
     <AppBar>
       <Toolbar>
         <div>
-          <CitiesSearch />
+          <CitiesSearch names={names} value={value} filterOptions={options} />
         </div>
       </Toolbar>
     </AppBar>
