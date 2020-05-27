64a8a7593edc9a9043c9768ba2d2ead4b0a2533f
Noah Pang <noahpang@cs.ucsb.edu>
2020-05-26 19:20:51 -0700

    np - separated test that city has correct information into multiple tests

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -12,7 +12,6 @@ describe("Authentication", () => {
   context("When I am not logged in", () => {
     it("has a login button", () => {
       cy.visit("http://localhost:3000");
-
       cy.get("[data-cy=login]").should("exist");
     });
   });
--- cypress/integration/city.spec.js
+++ cypress/integration/city.spec.js
@@ -1,28 +1,41 @@
 describe("Cities page", () => {
-  it("is reachable from the search bar", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("div[data-cy='searchfield']")
-      .parent()
-      .within(() => {
-        cy.get("input").type("Goleta");
-      });
-    cy.contains("Goleta").click({ force: true });
-    cy.url().should("contain", "/cities/Goleta");
+  context("Navigating to the page", () => {
+    it("is reachable from the search bar", () => {
+      cy.visit("http://localhost:3000");
+      cy.get("div[data-cy='searchfield']")
+        .parent()
+        .within(() => {
+          cy.get("input").type("Goleta");
+        });
+      cy.contains("Goleta").click({ force: true });
+      cy.url().should("contain", "/cities/Goleta");
+    });
   });
-
-  it("contains the correct data", () => {
-    cy.visit("http://localhost:3000/cities/goleta");
-    cy.get("[data-cy=location]").contains("Goleta, CA");
-    cy.get("[data-cy=population]").contains("Population:");
-    cy.get("[data-cy=carbon]").contains("Emits");
-    cy.get("[data-cy=waterquality]").contains("Water pH Level:");
-    cy.get("[data-cy=waterquality]")
-      .contains("Total Dissolved Solids:")
-      .contains("mg/L");
-    cy.get("[data-cy=waterquality]")
-      .contains("Specific Conductance:")
-      .contains("μS/cm");
-    cy.get("[data-cy=airquality]").contains("Today's Air Quality Index (AQI):");
+  context("On the page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/cities/goleta");
+    });
+    it("contains the location and population data", () => {
+      cy.get("[data-cy=location]").contains("Goleta, CA");
+      cy.get("[data-cy=population]").contains("Population:");
+    });
+    it("contains carbon data", () => {
+      cy.get("[data-cy=carbon]").contains("Emits");
+    });
+    it("contains water data", () => {
+      cy.get("[data-cy=waterquality]").contains("Water pH Level:");
+      cy.get("[data-cy=waterquality]")
+        .contains("Total Dissolved Solids:")
+        .contains("mg/L");
+      cy.get("[data-cy=waterquality]")
+        .contains("Specific Conductance:")
+        .contains("μS/cm");
+    });
+    it("contains air quality data", () => {
+      cy.get("[data-cy=airquality]").contains(
+        "Today's Air Quality Index (AQI):"
+      );
+    });
   });
 });
 
