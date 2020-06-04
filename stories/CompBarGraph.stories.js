76673af8712d51b66cef6305b793b58023a0dc53
Katelyn Zhang <krzhang@ucsb.edu>
2020-06-03 20:38:29 -0700

    kz - added usage of addon-knobs

--- stories/CompBarGraph.stories.js
+++ stories/CompBarGraph.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CompBarGraph from "../components/CompBarGraph";
-import { select, text, object, array, number } from "@storybook/addon-knobs";
+import { array, text } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import { AppBar, Toolbar } from "@material-ui/core";
 import useSWR from "swr";
@@ -14,22 +14,22 @@ export default {
 };
 
 export const AverageCompBarGraph = () => {
-  const labels = ["Average", "Chris Gaucho"];
-  const data = [3, 3];
-  const title = "Hour(s) of Screen Time Per Day";
+  const labels = array("Categories", ["Average", "Chris Gaucho"], ",");
+  const data = array("Data", [3, 3], ",");
+  const title = text("Title", "Hour(s) of Screen Time Per Day");
   return <CompBarGraph labels={labels} data={data} title={title} />;
 };
 
 export const AboveAverageCompBarGraph = () => {
-  const labels = ["Average", "Chris Gaucho"];
-  const data = [3, 6];
-  const title = "Hour(s) of Screen Time Per Day";
+  const labels = array("Categories", ["Average", "Chris Gaucho"], ",");
+  const data = array("Data", [3, 6], ",");
+  const title = text("Title", "Hour(s) of Screen Time Per Day");
   return <CompBarGraph labels={labels} data={data} title={title} />;
 };
 
 export const BelowAverageCompBarGraph = () => {
-  const labels = ["Average", "Chris Gaucho"];
-  const data = [3, 2];
-  const title = "Hour(s) of Screen Time Per Day";
+  const labels = array("Categories", ["Average", "Chris Gaucho"], ",");
+  const data = array("Data", [3, 2], ",");
+  const title = text("Title", "Hour(s) of Screen Time Per Day");
   return <CompBarGraph labels={labels} data={data} title={title} />;
 };
